4e82417928722670aef5f07529c223d01bad8c46
Ashik-Ahammad <ashik19ahammad@gmail.com>
2021-12-08 20:00:13 +0600

    16

--- src/Component/NotFound/NotFound.js
+++ src/Component/NotFound/NotFound.js
@@ -3,8 +3,9 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h4>Page you are looking for is currently not available. Sorry!!!</h4>
-        </div>
+            <img src="https://i.ibb.co/YfZZHXk/internet-network-warning-404-error-page-file-found-web-page-1150-48312.jpg" alt="" className="img-fluid w-50" />
+            <h4 className="mt-5 text-warning">Page you are looking for is currently not available. Sorry!!!</h4>
+        </div >
     );
 };
 
